b9bd84131a841f728ce3a456b428964b9f415fe0
48517 <kaja.wencel1997@gmail.com>
2023-03-21 20:11:43 +0100

    classAnimal

---
+++ main.py
@@ -0,0 +1,8 @@
+
+class Animal():
+    def __init__(self,owner,name,species,age,sex):
+        self.owner=owner
+        self.name=name
+        self.species=species
+        self.age=age
+        self.sex=sex
